850591ddd42ba0ea19ff65a5fd68196f55b49349
suleymandiker <suleyman.diker@gmail.com>
2022-06-08 19:50:38 +0300

    'test2'

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,29 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { ethers } from "ethers";
+
+let web3Modal: Web3Modal;
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      rpc: { 42: process.env.NEXT_PUBLIC_RPC_URL }, // required
+    },
+  },
+};
+
+if (typeof window !== "undefined") {
+  web3Modal = new Web3Modal({
+    cacheProvider: false,
+    providerOptions, // required
+  });
+}
+
+
 
 const Home: NextPage = () => {
   return (
